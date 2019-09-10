49c52b46415bf44ec98178ecf868521eeba85f42
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-10 23:12:09 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,44 +14,30 @@ def consolidate_cart(cart)
   new_hash
 end
 
+
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     if cart.keys.include? coupon[:item]
-      if cart[coupon[:item]][:count] >= coupon[:num]
-        new_name = "#{coupon[:item]} W/COUPON"
-        if cart[new_name]
-          cart[new_name][:count] += coupon[:num]
+      if cart[coupon[:item][:count]] >= coupon[:num]
+        itemname = "#{coupon[:item]} W?COUPON"
+        if cart[itemname]
+          cart[itemname][:count] += coupon[:num]
         else
-          cart[new_name] = {
+          cart[itemname] = {
             count: coupon[:num],
             price: coupon[:cost]/coupon[:num],
             clearance: cart[coupon[:item]][:clearance]
           }
         end
-        cart[coupon[:item]][:count] -= coupon[:num]
       end
     end
-  end
-  cart
+    cart
 end
 
 def apply_clearance(cart)
-  cart.keys.each do |item|
-    if cart[item][:clearance]
-      cart[item][:price] = (cart[item][:price]*0.80).round(2)
-    end
-  end
-  cart
+  # code here
 end
 
 def checkout(cart, coupons)
-  consol_cart = consolidate_cart(cart)
-  cart_with_coupons_applied = apply_coupons(consol_cart, coupons)
-  cart_with_discounts_applied = apply_clearance(cart_with_coupons_applied)
-
-  total = 0.0
-  cart_with_discounts_applied.keys.each do |item|
-    total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
-  end
-  total > 100.00 ? (total * 0.90).round : total
+  # code here
 end
